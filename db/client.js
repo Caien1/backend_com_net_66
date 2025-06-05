e32992e934985bd048a5d8e07b37a660e790f6b1
Caien1 <pulwama01@gmail.com>
2025-06-05 19:00:34 +0530

    prisma and signup

--- db/client.js
+++ db/client.js
@@ -1,29 +1,5 @@
 import {PrismaClient } from "@prisma/client";
 
 
-const prisma = new PrismaClient()
+export const prisma = new PrismaClient()
 
-
-async function createUser(){
-
-}
-
-// createUser().then(async ()=>{
-//     await prisma.$disconnect()
-// })
-
-
-
-async function main() {
-  // Run a simple query (e.g., list users)
-  const users = await prisma.user.findMany();
-  console.log('Users:', users);
-}
-
-main()
-  .then(() => prisma.$disconnect())
-  .catch(async (e) => {
-    console.error('Error:', e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
--- routes/user.js
+++ routes/user.js
@@ -1,23 +1,44 @@
 import express from 'express'
 import {UserValidation} from '../validation/auth_validation.js'
 
+import {prisma} from "../db/client.js"
+
 export const router = express.Router()
 
 
 
-router.post("/sign-up",(req,res)=>{
+router.post("/sign-up",async (req,res)=>{
 const body = req.body
-res.json({"message":"hello"})
-try{
-    UserValidation.parse(body)
-}catch(error){
-    throw new Error("You messed up")
-    
-}
 
+let verified = null
+    const emailAlreadyExists = await prisma.user.findFirst({
+        where:{
+            email: body.email
+        },
 
+    });
+    if(emailAlreadyExists!=null){
+        return res.status(400).json({"message":"Email Alredy Exists"})
+    }
 
-//console.log(username,email,password);
+try {
+        verified = UserValidation.parse(body);
+        console.log(verified);
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({ message: "Invalid Parameters" });
+    }
+
+    try {
+        const user = await prisma.user.create({
+            data: verified
+        });
+        console.log(user);
+        return res.status(201).json({ message: "User created" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Something went wrong" });
+    }
 
 })
 
