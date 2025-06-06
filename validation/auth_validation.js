22094ec3f90e9ffd2269950544349b9be6e7fac6
Caien1 <pulwama01@gmail.com>
2025-06-06 19:33:06 +0530

    validation for signup

--- validation/auth_validation.js
+++ validation/auth_validation.js
@@ -1,7 +1,14 @@
-import {z} from "zod/v4"
-export const  UserValidation = z.object({
+import { z } from "zod/v4"
+export const UserValidationSignUp = z.object({
     username: z.string().min(5).nonempty(),
-    email:z.email().nonempty(),
+    email: z.email().nonempty(),
     password: z.string().min(8).nonempty(),
 
 })
+
+export const UserValidationLogIn = z.object({
+
+    email: z.email().nonempty(),
+
+    password: z.string().min(8).nonempty(),
+});
