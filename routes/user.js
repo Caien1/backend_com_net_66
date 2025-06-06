d43c03813ebc0dc86caeeceb59ecb02bd6a6946c
Caien1 <pulwama01@gmail.com>
2025-06-06 19:32:31 +0530

    added singn-up

--- routes/user.js
+++ routes/user.js
@@ -1,33 +1,42 @@
 import express from 'express'
-import {UserValidation} from '../validation/auth_validation.js'
+import { UserValidationSignUp, UserValidationLogIn } from '../validation/auth_validation.js'
 
-import {prisma} from "../db/client.js"
+import { prisma } from "../db/client.js"
+import bycrypt from "bcrypt"
 
 export const router = express.Router()
 
 
 
-router.post("/sign-up",async (req,res)=>{
-const body = req.body
+router.post("/sign-up", async (req, res) => {
+    const body = req.body
 
-let verified = null
+    let verified = null
     const emailAlreadyExists = await prisma.user.findFirst({
-        where:{
+        where: {
             email: body.email
         },
 
     });
-    if(emailAlreadyExists!=null){
-        return res.status(400).json({"message":"Email Alredy Exists"})
+    if (emailAlreadyExists != null) {
+        return res.status(400).json({ "message": "Email Alredy Exists" })
     }
 
-try {
-        verified = UserValidation.parse(body);
+    try {
+        verified = UserValidationSignUp.parse(body);
         console.log(verified);
     } catch (error) {
         console.error(error);
         return res.status(400).json({ message: "Invalid Parameters" });
     }
+
+    const { password } = verified
+    const hashedPassword = await bycrypt.hash(password,10)
+    verified["password"] = hashedPassword;
+
+
+
+
 
     try {
         const user = await prisma.user.create({
@@ -43,7 +52,42 @@ try {
 })
 
 
-router.post("/sign-in",(req,res)=>{
-res.send("Sign in")
+router.post("/sign-in", async (req, res) => {
+    const body = req.body
+    try {
+        const verification = UserValidationLogIn.parse(body)
+        console.log(verification)
+    }
+    catch (error) {
+        return res.status(400).json({ message: "Invalid Email or password" })
+    }
+
+    try{
+        const user = await prisma.user.findFirst({
+            where: {email:body.email}
+        })
+        
+        const passwordCheck = await bycrypt.compare(body.password,user.password)
+        
+        if(passwordCheck){
+            
+        }
+
+        return res.status(400).json({message:"Well check the password"})
+
+
+
+        
+        
+    }
+    catch(error){
+        return res.status(400).json({message:"Invalid Email or Password"})
+    }
+
+
+
+
+
+
 })
 
